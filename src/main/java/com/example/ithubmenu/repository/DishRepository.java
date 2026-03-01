b09057cde2ce3ffd825c0ff169c9cf6dea55417a
Banan <akievkrytou47@gmail.com>
2026-03-01 15:41:34 +0300

    Delete src/main/java/com/example/ithubmenu directory

--- src/main/java/com/example/ithubmenu/repository/DishRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.ithubmenu.repository;
-
-import com.example.ithubmenu.entity.Dish;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DishRepository extends JpaRepository<Dish, Long> {
-}
